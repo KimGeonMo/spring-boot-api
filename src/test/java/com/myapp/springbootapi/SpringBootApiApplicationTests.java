b4ae8bfe7c6082d00f4da07197a8bebb38a30600
Kim Geon Mo <kpunh1542@naver.com>
2023-03-03 14:16:05 +0900

    Init

---
+++ src/test/java/com/myapp/springbootapi/SpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.springbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
